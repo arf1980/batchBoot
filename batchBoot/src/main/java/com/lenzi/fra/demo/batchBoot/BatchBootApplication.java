a251374afab0cb3348c3246bccd759549b7a94e8
arf1980 <fra.lenzi@gmail.com>
2015-09-01 12:28:06 +0200

    Primo Commit si git

---
+++ batchBoot/src/main/java/com/lenzi/fra/demo/batchBoot/BatchBootApplication.java
@@ -0,0 +1,12 @@
+package com.lenzi.fra.demo.batchBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchBootApplication.class, args);
+    }
+}
---
+++ batchBoot/src/test/java/com/lenzi/fra/demo/batchBoot/BatchBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lenzi.fra.demo.batchBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BatchBootApplication.class)
+public class BatchBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
